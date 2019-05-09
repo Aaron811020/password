69c4de06d15480c3e980109011aa5397d6796b57
AaronYen <clove2471@gmail.com>
2019-05-09 22:19:09 +0800

    modi while condition

--- password.py
+++ password.py
@@ -2,16 +2,17 @@
 
 password = "a123456"
 
-count = 1
-while count < 4:
+count = 0
+while True:
 	count = count +1
 	if input("請輸入密碼:") == password :
 		print("登入成功")		
 		break
 	else:
-		if 4 - count == 0:
+		if 3 - count == 0:
 			print("密碼錯誤3次, 請稍後再試")
+			break
 		else:
-			print("密碼錯誤! 還有 %d 次機會" % (4-count))
+			print("密碼錯誤! 還有 %d 次機會" % (3-count))
 
 	
